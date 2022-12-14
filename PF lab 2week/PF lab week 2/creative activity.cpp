d7b0f07c9141d4d2cbff274ce0c03d8578d5f1f0
Mohammad <junaidanjum748@gmail.com>
2022-12-14 15:41:06 +0500

    PF lab week2

---
+++ PF lab 2week/PF lab week 2/creative activity.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+main()
+{
+cout<<"        ______________________      "<<endl;
+cout<<"       /\\/                  \\/\\   "<<endl;
+cout<<"       ________________________      "<<endl;
+cout<<"       \\  \\                 / /     "<<endl;
+cout<<"        \\  \\               / /      "<<endl;
+cout<<"         \\  \\             / /       "<<endl;
+cout<<"          \\  \\           / /        "<<endl;
+cout<<"           \\  \\         / /         "<<endl;
+cout<<"            \\  \\       / /          "<<endl;
+cout<<"             \\  \\     / /           "<<endl;
+cout<<"              \\  \\   / /           "<<endl;
+cout<<"                \\ \\ / /           "<<endl;
+cout<<"                  \\ //            "<<endl;
+return 0;
+}         
---
+++ PF lab 2week/PF lab week 2/test.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+using namespace std;
+
+main()
+{
+cout<<"My name is MOhammad Junaid"<<endl<<"My roll no is 786";
+return 0;
+}
